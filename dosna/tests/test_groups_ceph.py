52ee4d010b04247d3b44edae0d034e686a05c200
GMW99 <gabryelmw@gmail.com>
2022-01-17 14:48:59 +0000

    Changing iocxt to a cls field
    This is done such that iocxt does not need to be defined in each test.

--- dosna/tests/test_groups_ceph.py
+++ dosna/tests/test_groups_ceph.py
@@ -30,11 +30,13 @@ class GroupTest(unittest.TestCase):
         dn.use(backend="ceph", engine="cpu")
         cls.connection_handle = dn.Connection("dosna", conffile="ceph.conf")
         cls.connection_handle.connect()
+        cls.iocxt = cls.connection_handle.instance.ioctx
 
     def setUp(self):
         if self.connection_handle.connected == False:
             self.connection_handle = dn.Connection("dosna", conffile="ceph.conf")
             self.connection_handle.connect()
+            self.iocxt = self.connection_handle.instance.ioctx
         self._started_at = time.time()
 
     @classmethod
@@ -50,25 +52,25 @@ class GroupTest(unittest.TestCase):
         self.connection_handle.disconnect()
 
     def check_group(
-        self, group, name, absolute_path, iocxt, attrs={}, links={}, datasets={}
+        self, group, name, absolute_path, attrs={}, links={}, datasets={}
     ):
         self.assertEqual(_SIGNATURE_GROUP, str(self.connection_handle.ioctx.read(PATH_SPLIT).decode()))
-        self.assertEqual(group.name, str(iocxt.get_xattr(name, "name").decode()))
+        self.assertEqual(group.name, str(self.iocxt.get_xattr(name, "name").decode()))
         self.assertEqual(group.name, name)
         self.assertEqual(
-            group.absolute_path, str(iocxt.get_xattr(name, "absolute_path").decode())
+            group.absolute_path, str(self.iocxt.get_xattr(name, "absolute_path").decode())
         )
         self.assertEqual(group.absolute_path, absolute_path)
         self.assertDictEqual(
-            group.attrs, str2dict(str(iocxt.get_xattr(name, "attrs").decode()))
+            group.attrs, str2dict(str(self.iocxt.get_xattr(name, "attrs").decode()))
         )
         self.assertEqual(group.attrs, attrs)
         self.assertDictEqual(
-            group.links, str2dict(str(iocxt.get_xattr(name, "links").decode()))
+            group.links, str2dict(str(self.iocxt.get_xattr(name, "links").decode()))
         )
         self.assertDictEqual(group.links, links)
         self.assertDictEqual(
-            group.datasets, str2dict(str(iocxt.get_xattr(name, "datasets").decode()))
+            group.datasets, str2dict(str(self.iocxt.get_xattr(name, "datasets").decode()))
         )
         self.assertDictEqual(group.datasets, datasets)
 
@@ -77,21 +79,20 @@ class GroupTest(unittest.TestCase):
 
     def test__create_group_object(self):
         name = "/A"
-        iocxt = self.connection_handle.instance.ioctx
         root = self.connection_handle.get_group(PATH_SPLIT)
         self.assertEqual(type(root), CpuGroup)
 
         A = root._create_group_object(name)
         # This should be a ceph group as it's access via a private method within CephGroup
         self.assertEqual(type(A), CephGroup)
-        self.check_group(A, name, name, iocxt)
+        self.check_group(A, name, name)
         attrs = {"A1": "V1"}
 
         A = root._create_group_object(name, attrs)
         # This should be a ceph group as it's access via a private method within CephGroup
         self.assertEqual(type(A), CephGroup)
-        self.check_group(A, name, name, iocxt, attrs)
+        self.check_group(A, name, name, attrs)
 
         # Check overwrites
         A = root._create_group_object(name)
-        self.check_group(A, name, name, iocxt)
+        self.check_group(A, name, name)
