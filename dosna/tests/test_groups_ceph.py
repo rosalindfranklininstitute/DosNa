1ec4567f9c7de4d6955c87b3522e5fdcedc7d141
GMW99 <gabryelmw@gmail.com>
2022-01-18 15:24:16 +0000

    Changing Access type
    As the data change whilst stored in the object store storing the current values in memory doesn't make sense therefore the data needs to be got/get every time.

--- dosna/tests/test_groups_ceph.py
+++ dosna/tests/test_groups_ceph.py
@@ -52,9 +52,7 @@ class GroupTest(unittest.TestCase):
                 self.connection_handle.ioctx.remove_object(obj.key)
         self.connection_handle.disconnect()
 
-    def check_group(
-        self, group, name, absolute_path, attrs={}, links={}, datasets={}
-    ):
+    def check_group(self, group, name, absolute_path):
         self.assertEqual(_SIGNATURE_GROUP, str(self.connection_handle.ioctx.read(PATH_SPLIT).decode()))
         self.assertEqual(group.name, str(self.ioctx.get_xattr(name, "name").decode()))
         self.assertEqual(group.name, name)
@@ -62,18 +60,7 @@ class GroupTest(unittest.TestCase):
             group.absolute_path, str(self.ioctx.get_xattr(name, "absolute_path").decode())
         )
         self.assertEqual(group.absolute_path, absolute_path)
-        self.assertDictEqual(
-            group.attrs, str2dict(str(self.ioctx.get_xattr(name, "attrs").decode()))
-        )
-        self.assertEqual(group.attrs, attrs)
-        self.assertDictEqual(
-            group.links, str2dict(str(self.ioctx.get_xattr(name, "links").decode()))
-        )
-        self.assertDictEqual(group.links, links)
-        self.assertDictEqual(
-            group.datasets, str2dict(str(self.ioctx.get_xattr(name, "datasets").decode()))
-        )
-        self.assertDictEqual(group.datasets, datasets)
+
 
     def test_root_group_exists(self):
         self.assertEqual(_SIGNATURE_GROUP, str(self.ioctx.read(PATH_SPLIT).decode()))
@@ -96,7 +83,7 @@ class GroupTest(unittest.TestCase):
         A = root._create_group_object(name, attrs)
         # This should be a ceph group as it's access via a private method within CephGroup
         self.assertEqual(type(A), CephGroup)
-        self.check_group(A, name, name, attrs)
+        self.check_group(A, name, name)
 
         # Check overwrites
         A = root._create_group_object(name)
@@ -109,7 +96,6 @@ class GroupTest(unittest.TestCase):
         self.assertEqual(type(group_obj), CpuGroup)
         self.check_group(group_obj, group_name, group_name)
         self.assertEqual(type(root), CpuGroup)
-        self.assertNotIn(group_name, root.links)  # TODO: make links a private field.
         self.assertIn(group_name, root.get_links())
 
     def test_create_existing_group(self):
@@ -126,9 +112,9 @@ class GroupTest(unittest.TestCase):
         attrs = {"A1": "V1"}
         group_obj = self.connection_handle.create_group(group_name, attrs)
         self.assertEqual(type(group_obj), CpuGroup)
-        self.check_group(group_obj, group_name, group_name, attrs)
+        self.check_group(group_obj, group_name, group_name)
+        self.assertEqual(attrs, group_obj.get_attrs())
         self.assertEqual(type(root), CpuGroup)
-        self.assertNotIn(group_name, root.links)
         self.assertIn(group_name, root.get_links())
 
     def test_create_subgroups(self):
@@ -137,13 +123,9 @@ class GroupTest(unittest.TestCase):
         groups = "/A/B/C"  # Expected /A -> /A/B -> /A/B/C
         group_obj = self.connection_handle.create_group(groups, attrs)  # Group /A/B/C as last group
         self.assertEqual(type(group_obj), CpuGroup)
-        self.check_group(group_obj, groups, "/A/A/B/A/B/C", attrs)
+        self.check_group(group_obj, groups, "/A/A/B/A/B/C")
         self.assertNotIn(groups, root.get_links())
         self.assertIn("/A", root.get_links())
-        # Check /A doesn't have attrs of /A/B/C
-        self.assertNotEqual(root.get_links()["/A"].target.attrs, attrs)
-        # Check /A/B doesn't have attrs of /A/B/C
-        self.assertNotEqual(root.get_links()["/A"].target.get_links()["/A/B"].target.attrs, attrs)
 
     def test_create_subgroup_with_existing_groups(self):
         root = self.connection_handle.get_group(PATH_SPLIT)
@@ -153,26 +135,21 @@ class GroupTest(unittest.TestCase):
         A_attrs = {"A1": "V1"}
         A = self.connection_handle.create_group(group_name, A_attrs)
         self.assertEqual(type(A), CpuGroup)
-        self.check_group(A, group_name, "/A", A_attrs)
+        self.check_group(A, group_name, "/A")
         self.assertIn("/A", root.get_links())
-        self.assertDictEqual(A.attrs, A_attrs)
+        self.assertDictEqual(A.get_attrs(), A_attrs)
         group_name = "/A/B"
         B = self.connection_handle.create_group(group_name)
         self.assertEqual(type(B), CpuGroup)
-        self.check_group(B, group_name, "/A/A/B", )
+        self.check_group(B, group_name, "/A/A/B")
         self.assertIn("/A/B", A.get_links())
 
         groups = "/A/B/C"  # Expected /A -> /A/B -> /A/B/C
         group_obj = self.connection_handle.create_group(groups, attrs)  # Group /A/B/C as last group
         self.assertEqual(type(group_obj), CpuGroup)
-        self.check_group(group_obj, groups, "/A/A/B/A/B/C", attrs)
+        self.check_group(group_obj, groups, "/A/A/B/A/B/C")
         self.assertNotIn(groups, root.get_links())
         self.assertIn("/A/B/C", B.get_links())
-        # Check /A doesn't have attrs of /A/B/C
-        self.assertNotEqual(root.get_links()["/A"].target.attrs, attrs)
-        self.assertEqual(root.get_links()["/A"].target.attrs, A_attrs)
-        # Check /A/B doesn't have attrs of /A/B/C
-        self.assertNotEqual(root.get_links()["/A"].target.get_links()["/A/B"].target.attrs, attrs)
 
     def test_create_subgroup_with_group(self):
         root = self.connection_handle.get_group(PATH_SPLIT)
@@ -232,8 +209,8 @@ class GroupTest(unittest.TestCase):
         with self.assertRaises(GroupNotFoundError):
             root.del_group("/A/B/C")
 
-        root.del_group("/A")
         root.create_group(groups)
+        root.del_group("/A")
         self.assertFalse(root.has_group("/A"))
         self.assertFalse(root.has_group("/A/B"))
         self.assertFalse(root.has_group("/A/B/C"))
